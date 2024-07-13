a1d98f813dea980842d970370e8f90f13b52f618
Roman Chernykh <professional255@gmail.com>
2024-07-13 15:02:25 +0700

    awdawd

--- format.ps1
+++ format.ps1
@@ -1,6 +1,7 @@
-#clang-format -i *.cpp *.h
+cd ./src
+clang-format -i *.cpp *.h
 
-Get-ChildItem -Path ./src/ -Directory -Recurse |
+Get-ChildItem -Path . -Directory -Recurse |
     foreach {
         cd $_.FullName
         &clang-format -i *.cpp *.h
